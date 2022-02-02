c3e939869790d83935a706f42b45f0569bb7b3a8
KepchykS <lololomivk@gmail.com>
2022-02-02 16:52:39 +0700

    404 RES

--- kepchyk.me/404.php
+++ kepchyk.me/404.php
@@ -1,2 +1,18 @@
-<h1>ERROR 404</h1>
-<h3>Test</h3>
+<<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Error 404</title>>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="<?=get_template_directory_uri();?>/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+        
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <h1>Error 404</h1>
+        <p>Старница не найдена</p>
+        
+        <script src="" async defer></script>
+    </body>
+</html>
